7f5506d14bf1ce1ec7da80704dbd1d0dfc0803dd
warofthefoxes <166264317+warofthefoxes@users.noreply.github.com>
2024-04-06 21:56:10 -0500

    Create startup.ps1

---
+++ startup.ps1
@@ -0,0 +1,21 @@
+$logFile = "./howtocomputer.txt"
+
+function Get-LogColor {
+    Param([Parameter(Position=0)]
+    [String]$LogEntry)
+
+    process {
+        if ($LogEntry.Contains("kick your fucking ass.")) {Return "Red"}
+        elseif ($LogEntry.Contains("====")) {Return "Blue"}
+	elseif ($LogEntry.Contains("++++++")) {Return "Blue"}
+        elseif ($LogEntry.Contains("username")) {Return "Green"}
+	elseif ($LogEntry.Contains("COMMAND:")) {Return "Green"}
+	elseif ($LogEntry.Contains("REPO:")) {Return "Green"}
+        else {Return "Yellow"}
+    }
+}
+
+get-content -wait $logFile | ForEach {Write-Host -ForegroundColor (Get-LogColor $_) $_}
+# Above borrowed and modified from https://stackoverflow.com/questions/6132140/colour-coding-get-content-results
+
+PAUSE
