5a86ec1f45c2ba3443f74b8a53e4e916d39bf0e8
Louie <166264317+warofthefoxes@users.noreply.github.com>
2024-04-16 22:25:35 -0500

    (does not run) implemented PSmenu, added functions
    Committing as backup / to show progress
    
    !! successfully implemented PSMenu !!
    
    Doesn't run properly. Lots of redundant functions and functions that don't implement PSMenu. Could probably nest/pipe some of these functions.

--- HowToComputer/startup.ps1
+++ HowToComputer/startup.ps1
@@ -1,16 +1,40 @@
 $HTComputer = "C:\Users\Louis\Desktop\StartupFiles\howtocomputer.txt"
+$libraryPath = "C:\Users\Louis\Desktop\StartupFiles\Library\"
+$YN = @("YES","NO")
+## Region Functions
 
-## Functions
+function folderMenu {
+    write-host "=== FOLDERS IN LIBRARY ==="
+    $folderOpts = @()
+    $folderOpts += Get-ChildItem -Path $libraryPath
+    $chosenDir = Show-Menu -MenuItems $folderOpts
+    Write-Host $chosenDir
 
+    $chosenFilepath = -join($libraryPath,$chosenDir)
+#    Write-Host $chosenFilepath
+    Clear-Host
+    }
+#adds files in chosen folder to array, adds array to psmenu
+function fileMenu 
+    {
+    Clear-Host
+    write-host ("=== FILES IN", $chosenDir, "===")
+    $Opts = @()
+    $Opts += Get-ChildItem -Path $chosenFilepath
+    $pickfile = Show-Menu -MenuItems $Opts
+    Clear-Host
+    }
+function displayText {
+    write-host ("TEXT IN",$pickFile)
+    get-content $chosenFilepath\$pickfile
+    }
 # establishes variables and changes dir to startdir
-# need to change this so the paths aren't hard-coded
-    # possibly create function to check for and create Library folder within current dir
-function ChangeDir {
+# need to add one-time script to set library dir
+    function ChangeDir {
     $StartDir = "C:\Users\Louis\Desktop\StartupFiles"
     $LibraryDir = "C:\Users\Louis\Desktop\StartupFiles\Library"
-    cd $StartDir
+    Set-Location $StartDir
 }
-
 # checks if PSMenu is installed, installs if not
 function checkInstallPSMenu {
 
@@ -22,64 +46,60 @@ function checkInstallPSMenu {
 }
 
 # asks if user wants to create a file, takes filename, checks for/appends .txt
+# takes contents, adds to file.
+# kind of a mess. could be more concise. 
 function FileCreate {
-    $FileYN = Read-Host -Prompt ">>>>> Would you like to create a text file? y/n"
-    If ($FileYN -imatch "y") {
-        
-        cd $LibraryDir
-        
-        $FileCreateName = Read-Host ">>>>> What should the filename be?"
-        $TxtYN = $FileCreateName -inotcontains ".txt"
-            if ($TxtYN = $true) {
-             $FileCreateName = -Join ($FileCreateName,".txt")
-             }
-        
+    write-host "Would you like to create a new file?"
+    $fileYN = Show-Menu -MenuItems $YN 
+    
+    if ($fileYN = "Y") {
+            Clear-Host
+            $FileCreateName = Read-Host ">>>>> What should the filename be?"
+            if ($FileCreateName -inotcontains ".txt") {
+                $FileCreateName = -Join ($FileCreateName,".txt")
+                }
+        Clear-Host
         $FileContents = Read-Host ">>>>> Please type your contents."
         New-Item $LibraryDir\"$FileCreateName"
         Add-Content -Path .\$FileCreateName -Value $FileContents
         }
+        # should change above to open file in notepad.exe ?
+        # do I need the below?
     elseIf ($FileYesNo -inotmatch "n" -and $FileYesNo -inotmatch "y" ) {
         Write-Host "OK"
+        }
     }
 
- }
-
-
- # asks if user wants to open a file, clears console and displays text
+function openFolderFileMenu {
+    $openYN = Show-Menu -MenuItems $YN
+    If ($openYN = 'Y') {
+        folderMenu
+        fileMenu
+        displayText
+    }
+    else {
+        
+    }
+}
 function FileYn {
     $FileYesNo = Read-Host -Prompt ">>>>> Would you like to open a text file? y/n"
      If ( $FileYesNo -imatch "y" ) {
-       cd "C:\Users\Louis\Desktop\StartupFiles\Library"
+      <# Set-Location "C:\Users\Louis\Desktop\StartupFiles\Library"
         # even after calling ChangeDir, $LibraryDir was unreadable. Why?
-       dir
+       Get-ChildItem
         # write cooler code to replace this / implement ps-menu
         $FileInput = Read-Host -prompt ">>>>> Which file?"
         $FileName = -join("$FileInput",".txt")
        Clear-Host
        displayColorText $FileName
+       #>
+       folderMenu
      }
      elseIf ($FileYesNo -imatch "n") {
         # $FileCreate = Read-Host ">>>>> Would you like to create a text file? y/n"
         FileCreate
      }
  }
-
- #function to test if input contains ".txt"
- #broken, needs cleaned
-<# $TextString = "no.txt"
-function ContainsTxt {
-    If ($TextString.Contains(".txt.")) {
-        $YN = TR
-      }
-    else {
-        $YN = FALSE
-      }
-}
-
-# ContainsTxt($TextString)
-Write-Host $YN
-#>
-
 
 # need to add a way to tell if the filename entered is valid without breaking the code
 # need to add a way to tell if filename was entered with extension other than .txt
@@ -107,17 +127,20 @@ function displayColorText {
     get-content $fileInput | ForEach {Write-Host -ForegroundColor (Get-textColor $_) $_}
 } 
 
+# Region Main
+
+Do {
 Clear-Host
 ChangeDir
 checkInstallPSMenu
+openFolderFileMenu
+# displayColorText $HTComputer
 
-displayColorText $HTComputer
+# FileYn 
+$Continue = Read-host -Prompt "Enter X to exit"
 
-# $null = Read-Host -Prompt "Press the ANY key"
-# change this to prompt for user input
+} while ( $Continue -notcontains "X")
 
-FileYn
-
-$null = Read-Host -Prompt "Press any key to close window."
+# $null = Read-Host -Prompt "Press any key to close window."
     # need better way to prevent the window from closing once the code completes
     # possibly use a loop as in previous java projects
